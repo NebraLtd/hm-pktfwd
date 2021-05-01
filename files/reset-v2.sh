115088e24e40ea5f6577cf756ddb619dab8acdb4
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-01 23:53:06 +0100

    Add in reset v2

--- files/reset-v2.sh
+++ files/reset-v2.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ ! -d "/sys/class/gpio/gpio22" ]; then
-    echo "22" > /sys/class/gpio/export
+if [ ! -d "/sys/class/gpio/gpio$1" ]; then
+    echo "$1" > /sys/class/gpio/export
 fi
-echo "out" > /sys/class/gpio/gpio22/direction
+echo "out" > /sys/class/gpio/gpio$1/direction
 sleep 2
-echo "1" > /sys/class/gpio/gpio22/value
+echo "1" > /sys/class/gpio/gpio$1/value
 sleep 2
-echo "0" > /sys/class/gpio/gpio22/value
+echo "0" > /sys/class/gpio/gpio$1/value
