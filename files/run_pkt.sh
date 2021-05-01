16c21add5f4adbc17eeb2cdeaacd380e915619a1
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-01 23:54:23 +0100

    Tidy up starting bash script.

--- files/run_pkt.sh
+++ files/run_pkt.sh
@@ -3,10 +3,5 @@
 #Run Packetforward
 #This script runs on the boot of the container
 
-
-
-#Reset all pins
-#./reset-pins.sh
-
 #Run python to do configuration and run the induvidual forwarders
 python3 -u configurePktFwd.py
