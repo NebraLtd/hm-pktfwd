722b1ddc3e446d77862428e4cf5930b3e765e135
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-01 23:40:13 +0100

    Lint bash scripts

--- buildfiles/compileSX1301.sh
+++ buildfiles/compileSX1301.sh
@@ -2,17 +2,17 @@
 
 mkdir -p /opt/iotloragateway
 mkdir -p /opt/iotloragateway/dev
-cd /opt/iotloragateway/dev
+cd /opt/iotloragateway/dev || exit
 
 git clone https://github.com/NebraLtd/lora_gateway.git
 git clone https://github.com/NebraLtd/packet_forwarder.git
 
-cd /opt/iotloragateway/dev/lora_gateway/libloragw
+cd /opt/iotloragateway/dev/lora_gateway/libloragw || exit
 make clean
 make -j 4
 
 echo "Packet Forwarder"
-cd /opt/iotloragateway/dev/packet_forwarder/
+cd /opt/iotloragateway/dev/packet_forwarder/ || exit
 make clean
 make -j 4
 
--- buildfiles/compileSX1302.sh
+++ buildfiles/compileSX1302.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-cd /opt/iotloragateway/dev
+cd /opt/iotloragateway/dev || exit
 
 #git clone https://github.com/NebraLtd/sx1302_hal.git
 wget https://github.com/NebraLtd/sx1302_hal/archive/V1.0.5.tar.gz
 tar -xzvf V1.0.5.tar.gz
 
-cd /opt/iotloragateway/dev/sx1302_hal-1.0.5
+cd /opt/iotloragateway/dev/sx1302_hal-1.0.5 || exit
 
 #Remove old files
 
