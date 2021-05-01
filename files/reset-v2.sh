10735e27aaeecabef7612995c529135b28fa9b5c
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-02 00:00:31 +0100

    Linting fixes for reset script

--- files/reset-v2.sh
+++ files/reset-v2.sh
@@ -3,8 +3,8 @@
 if [ ! -d "/sys/class/gpio/gpio$1" ]; then
     echo "$1" > /sys/class/gpio/export
 fi
-echo "out" > /sys/class/gpio/gpio$1/direction
+echo "out" > "/sys/class/gpio/gpio$1/direction"
 sleep 2
-echo "1" > /sys/class/gpio/gpio$1/value
+echo "1" > "/sys/class/gpio/gpio$1/value"
 sleep 2
-echo "0" > /sys/class/gpio/gpio$1/value
+echo "0" > "/sys/class/gpio/gpio$1/value"
