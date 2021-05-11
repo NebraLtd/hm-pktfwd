4797d5a661fbae2f9145241592b1642bb210e963
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-11 22:14:09 +0100

    Fix linting

--- buildfiles/compileSX1301.sh
+++ buildfiles/compileSX1301.sh
@@ -2,7 +2,7 @@
 echo "Compiling for $1"
 
 cd /opt/iotloragateway/dev/lora_gateway/libloragw || exit
-sed -i 's/spidev0.0/$1/g' src/loragw_spi.native.c
+sed -i "s/spidev0.0/$1/g" src/loragw_spi.native.c
 make clean
 make -j 4
 
@@ -11,4 +11,4 @@ cd /opt/iotloragateway/dev/packet_forwarder/ || exit
 make clean
 make -j 4
 
-cp -R /opt/iotloragateway/dev/packet_forwarder/lora_pkt_fwd/lora_pkt_fwd /opt/iotloragateway/packetforwarder/lora_pkt_fwd_$1
+cp -R "/opt/iotloragateway/dev/packet_forwarder/lora_pkt_fwd/lora_pkt_fwd" "/opt/iotloragateway/packetforwarder/lora_pkt_fwd_$1"
