223f3c98212893e8a6edca7a48696ceb09f56c61
Aaron Shaw <shawaj@gmail.com>
2022-12-31 19:43:53 +0000

    feat: add sx1257 reset capability for pycom
    add sx1257 reset capability for pycom

--- reset_lgw.sh
+++ reset_lgw.sh
@@ -27,6 +27,11 @@ else
     CONCENTRATOR_RESET_PIN=$2
 fi
 
+if [ -n "${SX125x_RESET_PIN_OVERRIDE+x}" ]; then
+    echo "SX125x_RESET_PIN_OVERRIDE parameter found, using this value from the environment (val=${SX125x_RESET_PIN_OVERRIDE})"
+    SX125x_RESET_PIN=${SX1257x_RESET_PIN_OVERRIDE}
+fi
+
 WAIT_GPIO() {
     sleep 0.1
 }
@@ -37,6 +42,12 @@ init() {
 
     # set GPIOs as output
     echo "out" > "/sys/class/gpio/gpio${CONCENTRATOR_RESET_PIN}/direction"; WAIT_GPIO
+    
+    if [ -n "${SX125x_RESET_PIN+x}" ]
+    then
+      echo "${SX125x_RESET_PIN}" > /sys/class/gpio/export; WAIT_GPIO
+      echo "out" > "/sys/class/gpio/gpio${SX125x_RESET_PIN}/direction"; WAIT_GPIO
+    fi
 }
 
 reset() {
@@ -49,6 +60,13 @@ reset() {
         echo "1" > "/sys/class/gpio/gpio${CONCENTRATOR_RESET_PIN}/value"; WAIT_GPIO
         echo "0" > "/sys/class/gpio/gpio${CONCENTRATOR_RESET_PIN}/value"; WAIT_GPIO
     fi
+
+    if [ -d "/sys/class/gpio/gpio${SX125x_RESET_PIN}" ]
+    then
+        echo "SX125x reset through GPIO${SX125x_RESET_PIN}..."
+        echo "1" > "/sys/class/gpio/gpio${SX125x_RESET_PIN}/value"; WAIT_GPIO
+        echo "0" > "/sys/class/gpio/gpio${SX125x_RESET_PIN}/value"; WAIT_GPIO
+    fi
 }
 
 term() {
@@ -56,6 +74,11 @@ term() {
     if [ -d "/sys/class/gpio/gpio${CONCENTRATOR_RESET_PIN}" ]
     then
         echo "${CONCENTRATOR_RESET_PIN}" > /sys/class/gpio/unexport; WAIT_GPIO
+    fi
+
+    if [ -d "/sys/class/gpio/gpio${SX125x_RESET_PIN}" ]
+    then
+        echo "${SX125x_RESET_PIN}" > /sys/class/gpio/unexport; WAIT_GPIO
     fi
 }
 
